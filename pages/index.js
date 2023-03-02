1347070affff2aac88735e301dce2d8e3a62c14b
Amrita Venkatraman <amritav.ca@gmail.com>
2023-03-01 20:08:26 -0800

    new post

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,8 @@ export default function Home( {allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p> Hi! My name is Amrita. I recently graduated from Stanford University, where I studied Computer Science and Psychology. I love reading, writing, and Rihanna. 
-          I currently live in sunny San Francisco, California. You can find me on <a href="https://twitter.com/iamrita98">Twitter</a>.
+        <p> Hi! My name is <a href="https://www.linkedin.com/in/amritavenkatraman/">Amrita</a>. I recently graduated from Stanford University, where I studied Computer Science and Psychology. I love reading, writing, and Rihanna. 
+          I currently live in San Francisco, California. You can find me on <a href="https://twitter.com/iamrita98">Twitter</a>.
         </p>
 
       </section>
