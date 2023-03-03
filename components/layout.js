deb554e3b61d69344e4dc18fc54daed080124617
Amrita Venkatraman <amritav.ca@gmail.com>
2023-03-02 17:24:08 -0800

    added contact bar on the bottom

--- components/layout.js
+++ components/layout.js
@@ -50,7 +50,7 @@ export default function Layout({ children, home }) {
                         />
 
                     </Link>
-                   {/* <h2 className={utilStyles.headingLg}>
+                    {/* <h2 className={utilStyles.headingLg}>
                         <Link href="/" className={utilStyles.colorInherit}>
                             {name}
                         </Link>
@@ -59,11 +59,52 @@ export default function Layout({ children, home }) {
             )}
         </header>
         <main>{children}</main>
-
         {!home && (
             <div className={styles.backToHome}>
                 <Link href="/"> Back To Home </Link>
             </div>
         )}
+        <div className={styles.row}>
+            <a className={styles.column} href="https://github.com/iamrita">
+                <Image
+                    priority
+                    unoptimized
+                    src="/images/github.png"
+                    height={20}
+                    width={20}
+                    alt=""
+                />
+            </a>
+            <a className={styles.column} href="mailto: amritav.ca@gmail.com">
+                <Image
+                    priority
+                    unoptimized
+                    src="/images/email.png"
+                    height={20}
+                    width={20}
+                    alt=""
+                />
+            </a>
+            <a className = {styles.column} href="https://www.linkedin.com/in/amritavenkatraman/">
+                <Image
+                    priority
+                    unoptimized
+                    src="/images/linkedin.png"
+                    height={20}
+                    width={20}
+                    alt=""
+                />
+            </a>
+            <a className = {styles.column} href="https://twitter.com/iamrita98">
+                <Image
+                    priority
+                    unoptimized
+                    src="/images/twitter.png"
+                    height={20}
+                    width={20}
+                    alt=""
+                />
+        </a>
+        </div>
     </div>);
 }
