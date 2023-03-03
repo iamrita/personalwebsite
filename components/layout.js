ac46682130a886851c791a57764bbdbbe0d0f66c
Amrita Venkatraman <amritav.ca@gmail.com>
2023-03-02 16:14:08 -0800

    umbrella ico

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,7 @@ export const siteTitle = "Amrita's Blog";
 export default function Layout({ children, home }) {
     return (<div className={styles.container}>
         <Head>
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/umbrella.ico" />
             <meta
                 name="description"
                 content="Amrita's musings"
