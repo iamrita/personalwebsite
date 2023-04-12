74b6299d68476080b9654264ea10a9faaea0b54c
Amrita <amritav.ca@gmail.com>
2023-04-11 17:10:43 -0700

    updated home page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Link from 'next/link';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts';
-import Date from '../components/date'
-
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -13,36 +12,46 @@ export async function getStaticProps() {
   return {
     props: {
       allPostsData,
-    }
-  }
+    },
+  };
 }
 
-export default function Home( {allPostsData }) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p> Hi! My name is <Link href={`/posts/about`}>Amrita</Link>. I recently graduated from Stanford University, where I studied Computer Science and Psychology. I love reading, writing, and Rihanna. 
-          I currently live in San Francisco, California. You can find me on <a href="https://twitter.com/iamrita98">Twitter</a> or check out my <a href="/files/resume_feb_2023.pdf" download>resume</a>.
+        <p>
+          {" "}
+          Hi! My name is <Link href={`/posts/about`}>Amrita</Link>. I recently
+          graduated from Stanford University, where I studied Computer Science
+          and Psychology. I currently live
+          in San Francisco, California and am looking for frontend/fullstack
+          roles in the healthcare, education, or public policy industry. You can
+          find me on <a href="https://twitter.com/iamrita98">Twitter</a> or
+          check out my{" "}
+          <a href="/files/resume_feb_2023.pdf" download>
+            resume
+          </a>
+          .
         </p>
-
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({id, date, title}) => (
+          {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-             <Link href={`/posts/${id}`}>{title}</Link>
-             <br/>
-             <small className={utilStyles.lightText}>
-              <Date dateString={date}/>
-             </small>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
