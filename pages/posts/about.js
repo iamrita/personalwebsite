76bd9a34c0b26ffc3f3039def2cab1b43d1b5227
iamrita <amritav.ca@gmail.com>
2024-02-19 22:14:45 -0800

    cleaned up about page

--- pages/posts/about.js
+++ pages/posts/about.js
@@ -3,10 +3,11 @@ import Head from 'next/head';
 import utilStyles from '../../styles/utils.module.css'
 import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import { useState, useEffect } from 'react';
+import { ca } from 'date-fns/locale';
 
 
 const khanfectionsCoords = {
-    name: "Khanfections",
+    name: "Khanfections - best breakfast sandwich in the city",
     coords: {
         lat: 37.759030942158205,
         lng: -122.41532890548739
@@ -14,7 +15,7 @@ const khanfectionsCoords = {
 };
 
 const sliceHouseCoords = {
-    name: "Slice House",
+    name: "Slice House - try the grandma style pizza, and get their punch card!",
     coords: {
         lat: 37.770024841643355,
         lng: -122.4475607321787
@@ -39,7 +40,7 @@ const mangroveCoords = {
 }
 
 const localEdition = {
-    name: "Local Edition",
+    name: "Local Edition - lovely live music",
     coords: {
         lat: 37.78786315864519,
         lng: -122.40322230519006
@@ -47,8 +48,11 @@ const localEdition = {
 }
 
 const noeValleyBakery = {
-    lat: 37.755188262266095,
-    lng: -122.43301709046229
+    name: "Noe Valley Bakery - ask for the Milky Way Bomb! (and go their on your birthday for free treats)",
+    coords: {
+        lat: 37.755188262266095,
+        lng: -122.43301709046229
+    }
 }
 
 const kingThai = {
@@ -79,8 +83,59 @@ const norcina = {
         lng: -122.43964357265975
     }
 }
+
+const booksmith = {
+    name: "The Booksmith",
+    coords: {
+        lat: 37.7695546814879,
+        lng: -122.4513557916966
+    }
+}
+
+const tankHill = {
+    name: "Tank Hill - go for sunset",
+    coords: {
+        lat: 37.76013428680032,
+        lng: -122.4476748168379
+    }
+}
+
+const buenaVistaPark = {
+    name: "Buena Vista Park",
+    coords: {
+        lat: 37.77070626452147,
+        lng: -122.44307111499039
+    }
+}
+
+const alamoSquarePark = {
+    name: "Alamo Square Park - best dogs in the city here",
+    coords: {
+        lat: 37.77650081427212,
+        lng: -122.43507649539087
+    }
+}
+
+const greenAppleBooks = {
+    name: "Green Apple Books",
+    coords: {
+        lat: 37.76569595005676,
+        lng: -122.46658494567305
+    }
+}
+
+const folioBooks = {
+    name: "Folio Books",
+    coords: {
+        lat: 37.75140903783516,
+        lng: -122.43091182665557
+    }
+}
 const bars = [abvCoords, localEdition, snug]
-const restaurants = [mangroveCoords, khanfectionsCoords, sliceHouseCoords, norcina, noodleHouse]
+const restaurants = [mangroveCoords, khanfectionsCoords, sliceHouseCoords, norcina, noodleHouse, kingThai]
+const bakeries = [noeValleyBakery]
+const bookstores = [booksmith, greenAppleBooks, folioBooks]
+const parks = [tankHill, alamoSquarePark, buenaVistaPark]
 
 
 export default function About() {
@@ -95,48 +150,73 @@ export default function About() {
 
     const handleCategoryChange = (event) => {
         const category = event.target.value
+        console.log(category)
+        switch (category) {
+            case "Restaurants":
+                setMarkers(restaurants.map(restaurant => (
+                    <Marker key={restaurant.name} position={restaurant.coords} title={restaurant.name} />
+                )));
+                break;
+            case "Bars":
+                setMarkers(bars.map(bar => (
+                    <Marker key={bar.name} position={bar.coords} title={bar.name} />
+                )
+                ))
+                break;
+            case "Bookstores":
+                setMarkers(bookstores.map(bookstore => (
+                    <Marker key={bookstore.name} position={bookstore.coords} title={bookstore.name} />
+                )
+                ))
+                break;
 
-        if (category === "Restaurants") {
-            setMarkers(restaurants.map(restaurant => (
-                <Marker key={restaurant.name} position={restaurant.coords} title={restaurant.name} />
-            )));
-        } else if (category === "Bars") {
-            setMarkers(bars.map(bar => (
-                <Marker key={bar.name} position={bar.coords} title={bar.name} />
-            )
-
-            ))
+            case "Parks":
+                setMarkers(parks.map(park => (
+                    <Marker key={park.name} position={park.coords} title={park.name} />
+                )
+                ))
+                break;
+            case "Bakeries":
+                setMarkers(bakeries.map(bakery =>
+                    (<Marker key={bakery.name} position={bakery.coords} title={bakery.name} />)))
+                break;
         }
+        console.log(markers)
 
         setSelectedCategory(category)
     };
 
 
     return <Layout>
-        <Head>
-            <title>About me</title>
-        </Head>
-        {/* Google Map */}
-
         <article>
             <h1 className={utilStyles.headingXl}>About Me</h1>
             <div className={utilStyles.aboutText}>
-                <p>Generally, I am a creator. When I was a kid, I loved writing stories and building worlds: something that has
-                    stayed with me as an adult but manifests itself now in different ways. In the same vein that I love to design and sew my own clothes,
-                    I also enjoy brainstorming and developing web and mobile applications. My goal is to use my experience and knowledge
-                    in Human-Computer Interaction to create engaging experiences for my audience.
+                <p>Below, I've compiled a list of my favorites - places, books, TV shows, music, and movies. I try to keep them updated when I can. Please
+                    contact me if you'd like to geek out over anything listed together!
                 </p>
-                <p>In my free time, I like to volunteer in my community. Currently, I am a counselor in training for the
-                    <a href="https://sftu.org/"> San Francisco Tenants Union </a> and I am on the board of <a href="https://afutureforeverychild.org/">AFEC</a>, an international non-profit.
-                    I also am an avid reader, and love watching TV shows and movies, especially with friends. I've listed some of my favorites below.
-                </p>
-                <h1 className={utilStyles.headingMd}>My favorite places in SF</h1>
+                <h1 className={utilStyles.headingMd}>Places in San Francisco</h1>
+                <p>Use the dropdown below to filter based on category, and hover over the pin to see the name and my comments!</p>
 
-                <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div style={{ display: 'flex', alignItems: 'center' ,  marginBottom: '10px',}}>
                     <label htmlFor="restaurantFilter"></label>
-                    <select id="restaurantFilter" value={selectedCategory} onChange={handleCategoryChange}>
+                    <select id="restaurantFilter" value={selectedCategory} onChange={handleCategoryChange}
+                         style={{
+                            padding: '8px 30px 8px 12px', /* Adjusted padding on the right side */
+                            fontSize: '16px',
+                            borderRadius: '4px',
+                            border: '1px solid #ccc',
+                            backgroundColor: '#fff',
+                            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+                            appearance: 'none',
+                            backgroundImage: `url('data:image/svg+xml;utf8,<svg viewBox="0 0 24 24" fill="%23333" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z" /><path d="M0 0h24v24H0z" fill="none" /></svg>')`,
+                            backgroundRepeat: 'no-repeat',
+                            backgroundPosition: 'right 10px center'
+                          }}>
                         <option value="Restaurants">Restaurants</option>
                         <option value="Bars">Bars</option>
+                        <option value="Parks">Parks</option>
+                        <option value="Bookstores">Bookstores</option>
+                        <option value="Bakeries">Bakeries</option>
                     </select>
                 </div>
                 <LoadScript
@@ -160,28 +240,33 @@ export default function About() {
                     <li>Homegoing by Yaa Gyasi</li>
                     <li>The Vanishing Half by Brit Bennett</li>
                     <li>Pachinko by Min Jin Lee</li>
-
-
                 </ul>
+
                 <h1 className={utilStyles.headingMd}>TV Shows</h1>
                 <ul><li>Severance (Apple TV)</li>
                     <li>Bojack Horseman (Netflix)</li>
                     <li>Atlanta (Hulu)</li>
                     <li>Insecure (HBO Max)</li>
-
-
-
+                    <li>Chernobyl (HBO Max)</li>
                 </ul>
+
                 <h1 className={utilStyles.headingMd}>Movies</h1>
                 <ul><li>The Farewell</li>
-                    <li>Interstellar</li>
                     <li>Marcel the Shell With Shoes On</li>
                     <li>The Sound of Metal</li>
-                    <li>Pride and Prejudice</li>
+                    <li>Pride and Prejudice (Kiera Knightley version)</li>
                     <li>Zindagi Na Milegi Dobara</li>
                     <li>A Real Pain</li>
+                </ul>
 
 
+                <h1 className={utilStyles.headingMd}>Albums</h1>
+                <ul><li>Ctrl by SZA</li>
+                    <li>Nothing Like the Sun by Sting</li>
+                    <li>The Miseducation of Lauryn Hill by Lauryn Hill</li>
+                    <li>Swimming by Mac Miller</li>
+                    <li>ANTI by Rihanna</li>
+                    <li>Channel Orange by Frank Ocean</li>
                 </ul>
             </div>
         </article>
