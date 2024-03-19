cfc22cf0d8ead05b2ea1a27b09fe2422ef590cc4
iamrita <amritav.ca@gmail.com>
2024-03-19 15:48:54 -0700

    some weird animation

--- pages/posts/connections.js
+++ pages/posts/connections.js
@@ -15,8 +15,6 @@ const medium =['cornea', 'iris', 'lens', 'retina']
 const hard =['flail', 'flounder', 'thrash', 'struggle']
 const difficult=['airplane','breakfast', 'haircut', 'eyeball']
 
-
-
 const mapSubmission = (submission) => {
     let arr = []
     for (let i = 0; i < 16; i++) {
@@ -29,13 +27,14 @@ const mapSubmission = (submission) => {
 
 function containSameElements(arr1, arr2) {
     return arr1.sort().join(',')=== arr2.sort().join(',')
-
 }
 
 export default function Connections() {
-    const [clickedSquares, setClickedSquares] = useState(Array(16).fill(false)); // sets the clicked state for all the ssquares to false 
+    const [clickedSquares, setClickedSquares] = useState(Array(16).fill(false));
     const [clickCount, setClickCount] = useState(0);
-
+    const [mistakes, setMistakes] = useState(3);
+    const [submissionAnimation, setSubmissionAnimation] = useState(false);
+    const [showBackgroundRectangle, setShowBackgroundRectangle] = useState(false);
 
     const handleClick = (index) => {
         if (clickCount < 4 || clickedSquares[index]) {
@@ -64,21 +63,33 @@ export default function Connections() {
         containSameElements(submission, hard) ||
         containSameElements(submission, medium)) {
             console.log("You got one!")
+            setSubmissionAnimation(true);
+            setTimeout(() => {
+                setSubmissionAnimation(false);
+                setShowBackgroundRectangle(true);
+            }, 2000);
         } else {
-            console.log("You've made a mistake")
+            setMistakes(mistakes - 1)
+            console.log(`You've got ${mistakes} chances left.`)
         }
-        
+
     };
 
     const squares = words.map((word, index) => (
         <div
             key={index}
-            className={`${utilStyles.square} ${clickedSquares[index] ? utilStyles.clicked : ''}`}
+            className={`${utilStyles.square} ${clickedSquares[index] ? utilStyles.clicked : ''} ${submissionAnimation && clickedSquares[index] ? utilStyles.submissionAnimation : ''}`}
             onClick={() => handleClick(index)}
         >
             {word}
         </div>
     ));
+
+    const backgroundRectangle = (
+        <div className={`${utilStyles.square} ${utilStyles.backgroundRectangle}`}>
+            types of fish
+        </div>
+    );
 
     return (
         <Layout>
@@ -86,6 +97,7 @@ export default function Connections() {
                 <title>Connections</title>
             </Head>
             <article>
+                {showBackgroundRectangle && backgroundRectangle}
                 <div className={utilStyles.grid}>{squares}</div>
                 <div className={utilStyles.buttons}>
                     <button className={utilStyles.square} onClick={handleSubmit}>Submit</button>
