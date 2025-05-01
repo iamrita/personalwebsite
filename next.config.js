a1e4dc6e79c1cf005f8a4f3e3947f4c695600465
iamrita <amritav.ca@gmail.com>
2025-05-01 01:16:35 -0700

    no longer need next export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
-module.exports = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+// module.exports = {
+//   output: "export",
+//   images: {
+//     unoptimized: true,
+//   },
+//   trailingSlash: true,
+// };
+
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   trailingSlash: true,
-};
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
